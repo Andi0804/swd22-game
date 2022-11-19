b19e5336473eaee5de69511f002fd81d63a49366
Your Name <you@example.com>
2022-11-19 11:57:06 +0100

    Added player movement, randomized tiles

--- core/src/main/java/at/compus02/swd/ss2022/game/factories/TileFactory.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/factories/TileFactory.java
@@ -1,26 +1,29 @@
 package at.compus02.swd.ss2022.game.factories;
 
 import at.compus02.swd.ss2022.game.gameobjects.GameObject;
-import at.compus02.swd.ss2022.game.gameobjects.Player;
 import at.compus02.swd.ss2022.game.gameobjects.Tile;
+import java.util.Random;
 
-public class TileFactory implements Factory{
+public class TileFactory implements Factory {
     private static TileFactory tileFactory;
 
-    public static TileFactory getInstance(){
+    public static TileFactory getInstance() {
         if (tileFactory == null)
             tileFactory = new TileFactory();
 
         return tileFactory;
     }
 
-    private TileFactory(){
+    private TileFactory() {
 
     }
 
     @Override
     public GameObject create(float x, float y) {
-        Tile tile = new Tile();
+
+        String[] imgPaths = { "tile_wall.png", "tile_gras.png", "tile_gravel.png", "tile_water.png" };
+
+        Tile tile = new Tile(imgPaths[new Random().nextInt(4)]);
         tile.setPosition(x, y);
         return tile;
     }
--- core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/Player.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/Player.java
@@ -4,17 +4,32 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Player implements GameObject{
+import at.compus02.swd.ss2022.game.input.GameInput;
+
+public class Player implements GameObject {
     private Texture image;
     private Sprite sprite;
 
-    public Player(){
+    public Player() {
         image = new Texture("character.png");
         sprite = new Sprite(image);
     }
+
     @Override
     public void act(float delta) {
 
+        if (GameInput.pressedKeys.contains(GameInput.keys.up)) {
+            moveUp();
+        }
+        if (GameInput.pressedKeys.contains(GameInput.keys.down)) {
+            moveDown();
+        }
+        if (GameInput.pressedKeys.contains(GameInput.keys.left)) {
+            moveLeft();
+        }
+        if (GameInput.pressedKeys.contains(GameInput.keys.right)) {
+            moveRight();
+        }
     }
 
     @Override
@@ -26,4 +41,21 @@ public class Player implements GameObject{
     public void draw(SpriteBatch batch) {
         sprite.draw(batch);
     }
+
+    public void moveUp() {
+        setPosition(sprite.getX(), sprite.getY() + 1);
+    }
+
+    public void moveDown() {
+        setPosition(sprite.getX(), sprite.getY() - 1);
+    }
+
+    public void moveRight() {
+        setPosition(sprite.getX() + 1, sprite.getY());
+    }
+
+    public void moveLeft() {
+        setPosition(sprite.getX() - 1, sprite.getY());
+    }
+
 }
